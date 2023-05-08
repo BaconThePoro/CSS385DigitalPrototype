a789ad9d970681d4df2c5ea26a62d9580874ec0f
Randgriz11 <jacobtea12@gmail.com>
2023-05-07 21:01:43 -0700

    Tutorial Text
    sdsd

---
+++ DigitalPrototype/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+  public GameObject tutorial;
+  private int counter = 0;
+  private GameObject toKobeActive;
+  private GameObject toKobePrevious;
+
+  
+  public void NextButton()
+  {
+      if (counter == 0)
+      {
+          toKobeActive = tutorial.transform.GetChild(counter).gameObject;
+          toKobeActive.SetActive(true);
+          counter++;
+      }
+      else
+      {
+          toKobeActive = tutorial.transform.GetChild(counter).gameObject;
+          toKobePrevious = tutorial.transform.GetChild(counter-1).gameObject;
+          toKobePrevious.SetActive(false);
+          toKobeActive.SetActive(true);
+          counter++;
+
+      }
+
+
+     if (counter == 5)
+     {
+         toKobeActive.SetActive(false);
+
+     }
+  }
+}
